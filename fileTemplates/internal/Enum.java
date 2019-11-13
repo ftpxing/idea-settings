36e0f82f1ee76f40c26cc10241426f852da02fa9
xingshengqiang <xingshengqiang@shuidihuzhu.com>
2019-11-13 23:34:50 +0800

    IU-2019.2 <xingshengqiang@bogon Create baseRefactoring.xml, ide.general.xml, _@user_Default.icls, CodeGlance.xml, project.default.xml, Enum.java, Interface.java, lombok-plugin.xml, Default.xml, find.xml, textmate_os.xml, filetypes.xml, colors.scheme.xml, KeyPromoterXSettings.xml, path.macros.xml, databaseDrivers.xml, AnnotationType.java, Default for macOS copy.xml, Default.xml, Class.java, jdk.table.xml, laf.xml, vcs.xml, databaseSettings.xml, keymap.xml, mybatis.xml, editor.xml, usageView.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ *
+ * @author ${USER}
+ * @date ${DATE} ${TIME}
+ **/
+public enum ${NAME} {
+}
